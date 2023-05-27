586a438560bdfb5ebe1e54aac7d880dc7730a551
Waal28 <iwalfaizul@gmail.com>
2023-05-27 12:29:09 +0700

    TASK 12

--- src/components/6.redux/Result.jsx
+++ src/components/6.redux/Result.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-const Result = (props) => {
+const Result = () => {
   const user = useSelector((state) => state.user);
 
   return (
